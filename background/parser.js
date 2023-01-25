6c4cc28c0c832ce848597cc151e2244bbd060942
Sourav Yadav <sourav.yadav@outlook.in>
2023-01-25 17:17:55 +0530

    Updated class Response_parse in background/parser.js to take orginal locations as input. Google API slightly changes original address resulting in a slight inconsistency!

--- background/parser.js
+++ background/parser.js
@@ -1,15 +1,15 @@
 class Response_parser {
-    constructor(response) {
+    constructor(response, locations) {
         this.size = response["destinationAddresses"].length; 
         this.mappings = new Map(); 
-        this.distanc_matrix = new Array(this.size); 
+        this.distance_matrix = new Array(this.size); 
 
         for(let i = 0 ; i < this.size; i++) {
-            this.distanc_matrix[i] = new Array(this.size); 
-            this.mappings.set(i, response["destinationAddresses"][i]); 
-
+            this.distance_matrix[i] = new Array(this.size); 
+            this.mappings.set(i, locations[i]); 
+            
             for(let j = 0; j < this.size; j++) {
-                this.distanc_matrix[i][j] = response["rows"][i]["elements"][j]["distance"]["value"]; 
+                this.distance_matrix[i][j] = response["rows"][i]["elements"][j]["distance"]["value"]; 
             }
         }
     }
